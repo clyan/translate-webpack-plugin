0326d4174170c43989e471e45d3e807fb73381d9
ywymoshi <2429335889@qq.com>
2022-02-20 21:01:14 +0800

    test: 修改测试案例，增加测试文字数量

---
+++ example/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="H啊哈哈" />
+    <test msg="测试" />
+    <div>
+      {{ text }}
+    </div>
+    <button @click="change()">转换</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import Test from './components/Test.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Test,
+  },
+  data() {
+    return {
+      text: 'A系鞋带',
+    };
+  },
+  methods: {
+    change() {
+      this.text = '联系';
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ example/vue.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require('@vue/cli-service');
+const TranslateWebpackPlugin = require('translate-webpack-plugin');
+module.exports = defineConfig({
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new TranslateWebpackPlugin({
+        translateApiUrl: 'http://127.0.0.1:8999/api',
+        from: 'zh-CN',
+        to: 'zh-TW',
+        separator: '|',
+        regex: /[\u4e00-\u9fa5]/g,
+        outputTxt: true,
+      }),
+    ],
+  },
+});
